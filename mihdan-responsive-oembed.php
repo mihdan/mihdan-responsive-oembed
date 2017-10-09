3b266f1710c3994dde5cb8b206773140bc4dc5df
mihdan <mikhail@kobzarev.com>
2017-10-09 12:50:46 +0300

    Добавил магических стилей

--- mihdan-responsive-oembed.php
+++ mihdan-responsive-oembed.php
@@ -20,14 +20,42 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return string
  */
-function mihdan_set_embed_wrapper( $html ) {
+function mihdan_responsive_oembed_set_wrapper( $html ) {
 
 	// Работаем только на синглах
 	if ( is_singular( ) ) {
-		$html = sprintf( '<div class="post-embed-wrapper">%s</div>', $html );
+		$html = sprintf( '<div class="mihdan-responsive-oembed-wrapper">%s</div>', $html );
 	}
 
 	return $html;
 }
-add_filter( 'oembed_result', 'mihdan_set_embed_wrapper' );
+add_filter( 'oembed_result', 'mihdan_responsive_oembed_set_wrapper' );
+
+/**
+ * Добавляем свои стили в шапку сайта,
+ * в которых и заключена вся "магия" решения
+ */
+function mihdan_responsive_oembed_add_styles() {
+	$custom_css = '
+		.mihdan-responsive-oembed-wrapper {
+			position: relative;
+			padding-bottom: 56.25%; /* 16:9 */
+			padding-top: 25px;
+			margin-bottom: 15px;
+			height: 0;
+			background-color: #ddd;
+		}
+		.mihdan-responsive-oembed-wrapper iframe,
+		.mihdan-responsive-oembed-wrapper object,
+		.mihdan-responsive-oembed-wrapper embed {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+	';
+	wp_add_inline_style( 'mihdan_responsive_oembed_styles', $custom_css );
+}
+add_action( 'wp_enqueue_scripts', 'mihdan_responsive_oembed_add_styles' );
 // eof;
